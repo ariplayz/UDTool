a0e35ecf3199ce76ee0303a8553aa627435ab508
Ari Cummings <ari@aricummings.com>
2026-02-16 17:45:40 -0800

    add versioning and improve upload/download feedback in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,30 @@ fn main() -> std::io::Result<()> {
     let operator = &args[1];
     let file_contents = fs::read(&args[2])?;
     let file_name = &args[2];
+    let version = "1.0.0";
 
     let client = reqwest::blocking::Client::new();
 
+    println!("UDTool v{version} by Ari Cummings");
+    println!();
+
     if operator == "upload" || operator == "Upload" {
+        println!("Uploading {file_name}...");
         let _res = client.post("https://UDTool.delphigamerz.xyz/{file_name}")
             .body(file_contents)
             .send()
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        println!("File successfully uploaded.");
+        println!("File URL: https://UDTool.delphigamerz.xyz/{file_name}");
     }
     else if operator == "download" || operator == "Download" {
-        let res = client.get("https://UDTool.delphigamerz.xyz/{file_name}")
+        println!("Downloading {file_name}...");
+        let _res = client.get("https://UDTool.delphigamerz.xyz/{file_name}")
             .send()
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        println!("Downloaded {file_name}...");
 
-        let content = res.text().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        let content = _res.text().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
         fs::write(&args[2], content)?;
     }
 
