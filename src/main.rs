d7ee82a5ce50952dcd00507ae9ed26c22119a993
Ari Cummings <ari@aricummings.com>
2026-02-16 17:05:58 -0800

    implement upload and download functionality in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,26 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let file_contents = fs::read(&args[1])?;
+    let operator = &args[1];
+    let file_contents = fs::read(&args[2])?;
+    let file_name = &args[2];
 
     let client = reqwest::blocking::Client::new();
-    let _res = client.post("https://UDTool.delphigamerz.xyz")
-        .body(file_contents)
-        .send()
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    if operator == "upload" || operator == "Upload" {
+        let _res = client.post("https://UDTool.delphigamerz.xyz/{file_name}")
+            .body(file_contents)
+            .send()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+    }
+    else if operator == "download" || operator == "Download" {
+        let res = client.get("https://UDTool.delphigamerz.xyz/{file_name}")
+            .send()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+        let content = res.text().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        fs::write(&args[2], content)?;
+    }
 
 
 
